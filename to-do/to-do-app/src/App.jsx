dcb7fc653394b2253113a95a23d0797866f08559
Absher K <absheroamac@gmail.com>
2024-12-16 18:44:14 +0530

    Initial commit

---
+++ to-do/to-do-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  //Title
+  //List of Items -> ( check box, item, description)
+  //input field to add new items at top
+  //checked items move down and strike through
+
+  return (
+    <>
+      <h1>To do App</h1>
+    </>
+  );
+}
+
+export default App;
