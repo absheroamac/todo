5de392a933e3241ecce8fbc788cc4aae42369bdd
Absher K <absheroamac@gmail.com>
2024-12-17 23:02:59 +0530

    fixed

--- to-do/to-do-app/src/App.jsx
+++ to-do/to-do-app/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   };
 
   const reducer = (state, action) => {
-    let id = null;
     switch (action.type) {
       case "trackInput":
         return { ...state, input: action.input };
